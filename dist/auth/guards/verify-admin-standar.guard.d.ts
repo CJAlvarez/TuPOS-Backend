5312d3776326563c5a7f85e751894bc5fbdfe3a4
CJAlvarez <contacto@cjalvarez.com>
2026-01-28 13:55:30 -0600

    Disted

--- dist/auth/guards/verify-admin-standar.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Admin } from '../../entities/admin.entity';
-export declare class VerifyAdminAdminGuard implements CanActivate {
-    private readonly adminModel;
-    constructor(adminModel: typeof Admin);
-    canActivate(context: ExecutionContext): Promise<boolean>;
-}
